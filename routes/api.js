80d4720ad3d869cb3e6194d79ceda632d370fdb0
Xiong Tianrui <121947202+Ihavenochoised@users.noreply.github.com>
2025-11-18 14:38:29 +0800

    Add env variable "PUBLIC_URL"

--- routes/api.js
+++ routes/api.js
@@ -5,7 +5,8 @@ import { fileURLToPath } from 'url';
 const router = express.Router();
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const filesDir = path.join(__dirname, '..', 'public');
+
+const BASE_URL = process.env.PUBLIC_URL || `http://localhost:${process.env.PORT || 3000}`;
 
 router.get('/', (req, res) => {
     res.json({ message: 'Welcome to the API 🚀' });
@@ -32,15 +33,13 @@ async function proxyRequest(req, res) {
     try {
         const { UUID } = req.body;
         if (!UUID) return res.status(400).json({ error: "missing UUID in body" });
-        
-        // Make the request to the external API (could be a POST, GET, etc.)
+
         const externalApiURL = 'https://kahoot.it/rest/kahoots/';
         const externalApiResponse = await fetch(externalApiURL + UUID);
         if (!externalApiResponse.ok) {
             throw new Error(`External API returned error: ${externalApiResponse.statusText}`);
         }
         const responseBody = await externalApiResponse.json();
-        // Send back the body from the external API
         res.status(externalApiResponse.status).json(responseBody);
     } catch (err) {
         console.error('Error in proxy route:', err);
@@ -66,7 +65,7 @@ async function htmlToPDF(req, res) {
         await page.setContent(htmlContent, { waitUntil: 'networkidle0' });
         await page.evaluate(() => {
               const base = document.createElement('base');
-              base.href = `http://localhost:${process.env.PORT}/`;
+              base.href = `${BASE_URL}/`;
               document.head.appendChild(base);
         });
         const pdfBuffer = await page.pdf();  
