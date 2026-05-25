359387fdaf2e4e980f21980b76fa38d3749a6ac3
Xiong Tianrui <121947202+Ihavenochoised@users.noreply.github.com>
2026-05-25 09:43:46 +0000

    Change server port

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use((req, res) => {
     res.status(404).sendFile(path.join(__dirname, "public/404.html"));
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 globalThis.PORT = PORT;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 
