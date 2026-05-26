bea152ff5b6a63349cc3f69322679d189e695236
Xiong Tianrui <121947202+Ihavenochoised@users.noreply.github.com>
2026-05-26 03:44:03 +0000

    Remove nix packages that are not used for Replit

--- replit.nix
+++ replit.nix
@@ -1,20 +1,11 @@
-{pkgs}: {
-  deps = [
-    pkgs.freetype
-    pkgs.fontconfig
-    pkgs.expat
-    pkgs.dbus
-    pkgs.glib
-    pkgs.cairo
-    pkgs.pango
-    pkgs.xorg.libXi
-    pkgs.xorg.libXcursor
-    pkgs.xorg.libXrandr
-    pkgs.xorg.libXdamage
-    pkgs.xorg.libXcomposite
-    pkgs.cups
-    pkgs.atk
-    pkgs.nss
-    pkgs.chromium
-  ];
+{ pkgs }:
+
+{
+    deps = [
+      pkgs.psmisc
+        pkgs.zip
+        pkgs.nodejs_20
+        pkgs.chromium
+        pkgs.noto-fonts-cjk-sans
+    ];
 }
